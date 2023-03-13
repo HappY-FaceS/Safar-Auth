80c69f5b8541d456cc9df1454c5909e6c2a2d599
HappY-FaceS <rishavkumarcgc@gmail.com>
2023-03-13 18:12:03 +0530

    Added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
-//const UserRepository = require('./repository/user-repository');
-// const {User} = require('./models/index');
-// const bcrypt = require('bcrypt');
+
+// const UserService = require('./services/user-service');
 
 const app = express();
 
@@ -19,14 +18,10 @@ const prepareAndStartServer = () => {
     app.listen(PORT, async () => {
         console.log(`Server Started on Port: ${PORT}`);
 
-        //const repo = new UserRepository();
-        //const response = await repo.getById(1);
-        //console.log(response);
-        
-        // const incomingpassword = 'f3uybfd8bic';
-        // const user = await User.findByPk(4);
-        // const response = bcrypt.compareSync(incomingpassword, user.password);
-        // console.log(response);
+        //const service = new UserService();
+        // console.log("new token is", service.createToken({email: 'rishavkumar3101@dummy.com', id: 1}));
+        // console.log("user is",service.verifyToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJpc2hhdmt1bWFyMzEwMUBkdW1teS5jb20iLCJpZCI6MSwiaWF0IjoxNjc4NzEwODk1LCJleHAiOjE2Nzg3OTcyOTV9.kBlNa1f1BAGroC8yGJc59BYXt8GJJO20BcNKrRdAkaU'));
+
     });
 }   
 
